472f2bad29597fbbd7b521e281754569a393ac2e
Omer Ozkan <omer@ozkan.io>
2018-01-27 17:56:16 +0300

    Implement CRUD methods of MovieFixture

--- src/main/java/tr/org/ab/spring/rest/videostore/movie/MovieFixture.java
+++ src/main/java/tr/org/ab/spring/rest/videostore/movie/MovieFixture.java
@@ -14,19 +14,43 @@ public class MovieFixture {
     private Map<String, Movie> movies = new ConcurrentHashMap<>();
 
     public MovieFixture() {
-        Movie movie = new Movie()
-//                .setId(UUID.randomUUID().toString())
-                .setId("id")
+        Movie movie1 = new Movie()
+                .setId(UUID.randomUUID().toString())
                 .setCountries(Collections.singletonList("USA"))
                 .setImdbId("tt0338013")
                 .setTitle("Eternal Sunshine of the Spotless Mind")
                 .setYear(2004)
                 .setGenres(Arrays.asList("Drama", "Romance", "Sci-Fi"))
                 .setImdbRating(8.3f);
-        movies.put(movie.getId(), movie);
+
+        movies.put(movie1.getId(), movie1);
+
+        Movie movie2 = new Movie()
+                .setId(UUID.randomUUID().toString())
+                .setCountries(Arrays.asList("USA", "UK"))
+                .setImdbId("tt1375666")
+                .setTitle("Inception")
+                .setYear(2010)
+                .setGenres(Arrays.asList("Action", "Adventure", "Sci-Fi"))
+                .setImdbRating(8.8f);
+
+        movies.put(movie2.getId(), movie2);
     }
 
     public Movie getMovie(String uuid) {
         return movies.get(uuid);
+    }
+
+    public void addMovie(Movie movie) {
+        movie.setId(UUID.randomUUID().toString());
+        movies.put(movie.getId(), movie);
+    }
+
+    public void updateMovie(String id, Movie movie) {
+        movies.put(id, movie);
+    }
+
+    public void deleteMovie(String id) {
+        movies.remove(id);
     }
 }
